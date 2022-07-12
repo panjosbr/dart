46b9fe94f1a7b519026f748cb733b55808a45e11
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2022-07-12 10:08:19 -0300

    1 - Introducao a variaveis

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# dart
-repositorio com conteudos basicos sobre dart
+# Curso de Dart
+
+Repositório com conteudos basicos sobre dart
+
+Curso by Deivid Willyan
+https://www.youtube.com/watch?v=kRIWUk-thDM&list=PLRpTFz5_57cseSiszvssXO7HKVzOsrI77&index=2
---
+++ variaveis.dart
@@ -0,0 +1,14 @@
+void main() {
+  // 1 - Introdução a Variaveis
+  String nome = 'Panjos';
+  print(nome);
+
+  int valor = 10;
+  print(valor);
+
+  bool ehVerdadeiro = true;
+  print(ehVerdadeiro);
+
+  List<String> listaDePalavras = ['Yoru', 'Omen', 'Sage'];
+  print('${listaDePalavras[0]} - ${listaDePalavras[2]}');
+}
