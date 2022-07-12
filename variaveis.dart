8564a8df48e64dfee6c34411a756d80128f8b013
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2022-07-12 10:25:14 -0300

    2 - Introducao null-safety

--- variaveis.dart
+++ variaveis.dart
@@ -11,4 +11,16 @@ void main() {
 
   List<String> listaDePalavras = ['Yoru', 'Omen', 'Sage'];
   print('${listaDePalavras[0]} - ${listaDePalavras[2]}');
+
+  // 2 - Introducao ao null-safety
+
+  String? nomeEmpresa; // ? declara que a variavel pode ser null
+  nomeEmpresa = 'ABC';
+  print(nome!); // ! garante que essa variavel nao vai receber null
+  nomeEmpresa = null;
+
+  late String sobrenome;
+  sobrenome = 'xpto';
+  print(sobrenome);
+  // sobrenome = null; nao aceita esse tipo de atribuicao
 }
